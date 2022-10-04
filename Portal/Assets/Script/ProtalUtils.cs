1a5bb0fbe2ddc844d67ac49b468ea270f2174e71
karthikeyan <41743913+Saikarthi@users.noreply.github.com>
2022-10-05 01:04:24 +0530

    idel and move animation done

---
+++ Portal/Assets/Script/ProtalUtils.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Protal
+{
+    [System.Serializable]
+    public class GunTransform
+    {
+        public Vector3 Pos;
+        public Quaternion Rot;
+    }
+    
+}
